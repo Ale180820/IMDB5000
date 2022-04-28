b169ce1be455cb3063d9427cdb86d19d4bea9d95
Ale180820 <aylinneart@gmail.com>
2022-04-27 22:43:01 -0600

    Test post

--- Front IMDB5000/src/com/url/dreamTeam/Main.java
+++ Front IMDB5000/src/com/url/dreamTeam/Main.java
@@ -1,5 +1,17 @@
 package com.url.dreamTeam;
 
+import org.apache.http.HttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
 public class Main {
 
     public static void main(String[] args) {
@@ -7,6 +19,29 @@ public class Main {
     }
 
     public void movieRating(){
+        System.out.println("");
+        CloseableHttpClient httpclient = HttpClients.createDefault();
+        HttpGet httpget = new HttpGet("http://127.0.0.1:5000/countries");
+        try {
+            HttpResponse httpresponse = httpclient.execute(httpget);
+            var entity = httpresponse.getEntity();
+            StringBuilder builder = new StringBuilder();
 
+            if (entity != null) {
+                InputStream inputStream = entity.getContent();
+                var bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
+                for (String line = null; (line = bufferedReader.readLine()) != null;) {
+                    builder.append(line).append("\n");
+                }
+                //Exception getting thrown in below line
+                JSONArray jsonArray = new JSONArray(builder.toString());
+                for (int i = 0; i < jsonArray.length(); i++) {
+                    JSONObject jsonObject = jsonArray.getJSONObject(i);
+                    System.out.println(jsonObject);
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
