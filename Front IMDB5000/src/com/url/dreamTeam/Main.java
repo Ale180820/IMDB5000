3bd4cba9d2007572f0a3d24024862350df264709
Marcela Estrada <57819286+marcelaestrada@users.noreply.github.com>
2022-04-27 23:43:50 -0600

    createProfile

--- Front IMDB5000/src/com/url/dreamTeam/Main.java
+++ Front IMDB5000/src/com/url/dreamTeam/Main.java
@@ -1,12 +1,58 @@
 package com.url.dreamTeam;
 
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.json.*;
+
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
     }
 
+    public static String createProfile(String username, String password){
+
+        CloseableHttpClient client = HttpClients.createDefault();
+        HttpPost httpPost = new HttpPost("http://127.0.0.1:5000/countries");
+        JSONObject json = new JSONObject();
+
+        // json
+        json.put("username", username);
+        json.put("password",password);
+        System.out.print(json);
+
+        StringEntity entity = null;
+        try {
+            entity = new StringEntity(json.toString());
+            httpPost.setEntity(entity);
+            httpPost.setHeader("Accept", "application/json");
+            httpPost.setHeader("Content-type", "application/json");
+            CloseableHttpResponse response = client.execute(httpPost);
+            client.close();
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        } catch (ClientProtocolException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return json.toString();
+    }
+
+    public static boolean loginValidation(){
+        return true;
+    }
+
     public void movieRating(){
 
     }
+
 }
